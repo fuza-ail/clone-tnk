cca76cb220261a14a2ae3156f0dade7c236b2d74
fuza-ail <mfuzail1991@gmail.com>
2020-07-28 21:41:21 +0700

    feat: adding GTM

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { LoanService } from './services/loan.service';
-import {CustomerDetailService} from './services/customer-detail.service'
+import {CustomerDetailService} from './services/customer-detail.service';
+import {Router, NavigationEnd} from '@angular/router';
+import {filter} from 'rxjs/operators'
+
+declare var gtag;
+
 
 @Component({
   selector: 'app-root',
@@ -9,8 +14,16 @@ import {CustomerDetailService} from './services/customer-detail.service'
   providers:[LoanService,CustomerDetailService]
 })
 export class AppComponent {
-  constructor(){
 
+  constructor( /*router:Router*/){
+    // const navEndEvents = router.events.pipe(
+    //   filter(event=> event instanceof NavigationEnd)
+    // )
+    // navEndEvents.subscribe((event:NavigationEnd)=>{
+    //   gtag("config", "UA-173914111-1",{
+    //     'page_path':event.urlAfterRedirects
+    //   });
+    // })
   }
 
 }
